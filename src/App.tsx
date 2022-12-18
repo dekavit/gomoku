fc8a76ad52c9737f5819688a22ea2d96cdf0a1e0
dekavit <r120317@g.kobe-kosen.ac.jp>
2022-12-18 09:47:21 +0900

    ボタン設置　

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import internal from 'stream';
+
+class masume {
+  count: number;
+  str: string;
+  constructor() {
+    this.count = -1;
+    this.str = '未';
+  }
+
+  push_button(turn: number) {
+    if (this.count == -1) {
+      this.count = turn;
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        <LikeButton />
       </header>
     </div>
   );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(false);
+  const [str, setStr] = useState('黒');
+  const handleClick = () => {
+    setCount(!count);
+    if (count == true) setStr('白');
+    else setStr('黒');
+  };
+  return (<span className='likeButton' onClick={handleClick}>{str}</span>)
 }
 
 export default App;
